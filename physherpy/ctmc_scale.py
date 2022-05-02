Fix and improve gradient calculation
Allow the node height jacobian to be included or excluded in treelikelihood
Allow the substitution rate to be fixed

physherpy/ctmc_scale.py
@@ -3,6 +3,7 @@ import torchtree.distributions.ctmc_scale
 from torchtree.core.abstractparameter import AbstractParameter
 from torchtree.typing import ID
 
+import physherpy.physher.gradient_flags as flags
 from physherpy.interface import Interface
 from physherpy.physher import CTMCScaleModel as PhysherCTMCScaleModel
 
@@ -36,27 +37,47 @@ class CTMCScaleAutogradFunction(torch.autograd.Function):
         ctx.inst = inst
         ctx.save_for_backward(rates, internal_heights)
 
+        ctmc_model, tree_model = models
+
+        # the rates can be fixed so its shape can be different from internal_heights
+        # depending on the number of samples. For a strict clock with n internal nodes
+        # we can have: rates.shape==[1] and internal_heights.shape==[100,n]
+        if rates.dim() == 1:
+            ctmc_model.update(0)
+            if internal_heights.requires_grad and not rates.requires_grad:
+                inst.request_gradient([flags.TREE_RATIO])
+
+        # MAP or HMC
+        if internal_heights.dim() == 1:
+            internal_heights = internal_heights.unsqueeze(0)
+
         log_probs = []
         grads = []
-        for i in range(rates.shape[0]):
-            for model in models:
-                model.update(i)
+        for i in range(internal_heights.shape[0]):
+            if rates.dim() > 1:
+                ctmc_model.update(i)
+            tree_model.update(i)
+
             log_probs.append(torch.tensor([inst.log_likelihood()]))
-            if rates.requires_grad:
+            if internal_heights.requires_grad:
                 grads.append(torch.tensor(inst.gradient()))
-        ctx.grads = torch.stack(grads) if rates.requires_grad else None
+        ctx.grads = torch.stack(grads) if internal_heights.requires_grad else None
         return torch.stack(log_probs)
 
     @staticmethod
     def backward(ctx, grad_output):
         (
-            internal_heights,
             rates,
+            internal_heights,
         ) = ctx.saved_tensors
         internal_heights_grad = (
-            ctx.grads[..., internal_heights.shape[-1] :] * grad_output
+            ctx.grads[..., : internal_heights.shape[-1]] * grad_output
         )
-        rates_grad = ctx.grads[..., : internal_heights.shape[-1]] * grad_output
+        if rates.requires_grad:
+            rates_grad = ctx.grads[..., internal_heights.shape[-1] :] * grad_output
+        else:
+            rates_grad = None
+
         return (
             None,  # inst
             None,  # model

physherpy/tree_likelihood.py
@@ -10,6 +10,7 @@ from torchtree.evolution.substitution_model.abstract import SubstitutionModel
 from torchtree.evolution.tree_model import TreeModel
 from torchtree.typing import ID
 
+import physherpy.physher.gradient_flags as flags
 from physherpy.physher import TreeLikelihoodModel as PhysherTreeLikelihood
 from physherpy.utils import flatten_2D
 
@@ -139,6 +140,8 @@ class TreeLikelihoodModel(CallableModel):
         clock_model = None
         if BranchModel.tag in data:
             clock_model = process_object(data[BranchModel.tag], dic)
+            tree_model.zero_jacobian = include_jacobian
+
         return cls(
             id_,
             alignment,
@@ -188,12 +191,28 @@ class TreeLikelihoodFunction(torch.autograd.Function):
             mu,
         )
 
+        rate_need_update = True
+        if clock_rates is not None and clock_rates.dim() == 1:
+            models[3].update(0)
+            rate_need_update = False
+
+            if branch_lengths.requires_grad and not clock_rates.requires_grad:
+                physher_flags = [flags.TREELIKELIHOOD_FLAG_TREE]
+                if subst_rates is not None or subst_frequencies is not None:
+                    physher_flags.append(flags.TREELIKELIHOOD_FLAG_SUBSTITUTION_MODEL)
+                if weibull_shape is not None:
+                    physher_flags.append(flags.TREELIKELIHOOD_FLAG_SITE_MODEL)
+                inst.request_gradient(physher_flags)
+
         log_probs = []
         grads = []
         branch_lengths_flatten = flatten_2D(branch_lengths)
         for i in range(branch_lengths_flatten.shape[0]):
-            for model in models:
+            for model in models[:3]:
                 model.update(i)
+            if rate_need_update:
+                models[3].update(i)
+
             log_probs.append(torch.tensor([inst.log_likelihood()]))
             if branch_lengths.requires_grad:
                 grads.append(torch.tensor(inst.gradient()))